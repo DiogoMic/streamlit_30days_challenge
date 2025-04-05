f2170c2eb4149c01689fe4b2781d1e3f750c2cbc
Michael Diogo <89931817+DiogoMic@users.noreply.github.com>
2025-04-05 11:51:35 +0000

    day 1 -- 4

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,21 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.write ("#")
+if st.button("Hello, I am a Software Engineer"):
+    st.write("Welcome to my Blog😙")
+    st.write("I am a Software Engineer with a passion for building beautiful and functional applications.")
+    st.write("I am a Software Engineer with a passion for building beautiful and functional applications.")
+    st.write("I am a Software Engineer with a passion for building beautiful and functional applications.")
+elif st.button("Welcome to my Blog😙"):
+    st.write("Here are some of my blogs you can read about")
+    st.header ('Self Worth')
+    st.write ("Don't let your career define your identity-who you are extends beyond what you do for a living.")
+    st.write ("In a society that often equates self-worth with professional success, it's crucial to remember that our value isn't solely defined by our careers. We are multifaceted individuals with diverse interests, passions, and qualities that contribute to our identity. Embracing this holistic view of ourselves allows us to cultivate a sense of self-worth that transcends job titles and accolades.")
+    st.write ("By recognizing that our worth is inherent and not contingent upon external achievements, we can foster a healthier relationship with our careers. This perspective encourages us to pursue our passions, nurture our relationships, and engage in activities that bring us joy, regardless of their professional significance.")
+    st.write ("Ultimately, our self-worth is a reflection of our character, values, and the positive impact we have on others. By embracing this broader understanding of identity, we can navigate our careers with confidence and authenticity, knowing that we are so much more than what we do for a living.")
+    st.write ("Remember, you are not just your job title; you are a unique individual with a rich tapestry of experiences and qualities that make you who you are.")
+    st.write ("Embrace your multifaceted identity and let it shine in all aspects of your life.")
+    st.write ("#")
+    st.write ("Thank you for reading this short story. I hope you enjoyed it. If you have any questions or feedback, please feel free to reach out to me.")
+else:
+    st.write("goodbye")
